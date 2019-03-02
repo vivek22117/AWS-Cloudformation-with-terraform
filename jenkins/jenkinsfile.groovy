37b7137216516f942161bd3824068c298819da00
Vivek Mishra <vivekmishra22117@gmail.com>
2019-03-02 13:44:28 +0530

    Added jenkins file for automation

---
+++ jenkins/jenkinsfile.groovy
@@ -0,0 +1,69 @@
+pipeline {
+    agent any
+
+    options {
+        timestamps()
+    }
+    parameters {
+        string(name: 'VPC_NAME', defaultValue: 'vpc-subnet-network-by-vivek', description: 'Name of VPC Created')
+        string(name: 'REGION', defaultValue: 'us-east-1', description: 'AWS region specified')
+        string(name: 'WORKSPACE', defaultValue: 'development', description: 'worspace to use in Terraform')
+    }
+    environment {
+        TF_IN_AUTOMATION = "true"
+        AWS_METADATA_URL = "http://169.254.169.254:80/latest"
+        AWS_METADATA_TIMEOUT = "2s"
+    }
+
+    stages {
+        stage('role-&-policy-Init') {
+            steps {
+                dir('IncidetResponse-with-Lambda/access/') {
+                    script {
+                        sh "terraform --version"
+                        sh "terraform init"
+                        sh "whoami"
+
+                    }
+                }
+            }
+        }
+    }
+    stage('role-&-policy-plan') {
+        steps {
+            dir('IncidetResponse-with-Lambda/access/') {
+                script {
+                    try {
+                        sh "terraform workspace new ${param.WORKSPACE}"
+                    } catch (err){
+                        sh "terraform workspace select ${param.WORKSPACE}"
+                    }
+                    sh "terraform plan -var 'region=${param.REGION}' -out terraform-role-policy.tfplan;echo \$? > status"
+                    stash name: "terraform-role-policy-plan", includes: "terraform-role-policy.tfplan"
+                }
+            }
+        }
+        stage('role-&-policy-apply') {
+            steps {
+                dir('IncidetResponse-with-Lambda/access/') {
+                    script {
+                        def apply = false
+
+                        try {
+                            input message: 'confirm apply', ok: 'Apply config'
+                            apply = true;
+                        } catch (err) {
+                            apply = false
+                            sh "terraform destroy -var 'region=${param.REGION}' -force"
+                            currentBuild.result = 'UNSTABLE'
+                        }
+                        if(apply){
+                            unstash "terraform-role-policy-plan"
+                            sh "terraform apply terraform-role-policy.tfplan"
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
